58556d8bcb87653d050c57ee260eeae0485c9b2b
CarlosGDCJ <32521936+CarlosGDCJ@users.noreply.github.com>
2023-11-25 21:27:00 -0300

    Create graph

--- micrograd/engine.py
+++ micrograd/engine.py
@@ -1,3 +1,6 @@
+from graphviz import Digraph
+
+
 class Value:
     """Differentiable float"""
 
@@ -5,14 +8,32 @@ class Value:
         self.data = value
         self.parents = parents
         self.op = op
+        self.grad = 0
+        # Backward is a function because it only starts after the forward propagation
+        # We can't compute during it, it's a callback
+
+        # We use the current gradient to set the gradients of the parents
+        # At the very end of the computational graph, the gradient of the last node will be 1
+        # That's the base case
+        # From then we see which operation originated the last node
+
+        # --> l is the last (dl/dl = 1)
+        # e.g.: a + b = c
+        #       c * d = l
+
+        #       Contribution of c and d
+        #       dl/dc = dl/dl * dl/dc = 1 * d = d
+        #       dl/dd = dl/dl * dl/dd = 1 * c = c
+
+        #       Contribution of a and b
+        #       dl/da = dl/dc * dc/da = d * 1 = d
+        #       dl/db = dl/dc * dc/db = d * 1 = d
+
+        # We used the gradient of l to set the gradients of its parents (d and c)
+        # We used the gradient of c to set the gradients of its parents (a and b)
+        self._backward = lambda: None
 
     def __repr__(self):
-        if self.parents:
-            return (
-                f"Value: {self.data} Created by {self.op} applied to"
-                f" {[x.data for x in self.parents]}"
-            )
-
         return f"Value: {self.data}"
 
     def __add__(self, other):
@@ -41,9 +62,50 @@ class Value:
     def __truediv__(self, other):
         return self * (other**-1)
 
+    def __rtruediv__(self, other):
+        # can't do the standard self / other on r methods because unlike
+        # multiplication, division is not commutative
+        return (self**-1) * other
+
     def __pow__(self, other):
         assert isinstance(other, (int, float)), "Exponent not supported"
         return Value(self.data**other, parents=(self,), op="**")
 
     def __neg__(self):
         return -1 * self
+
+    def _make_graph(self):
+        nodes, edges = set(), set()
+
+        def explore(node):
+            if node not in nodes:
+                nodes.add(node)
+                for parent in node.parents:
+                    edges.add((parent, node))
+                    explore(parent)
+
+        explore(self)
+        return nodes, edges
+
+    # https://github.com/karpathy/micrograd/blob/master/trace_graph.ipynb
+    def draw_graph(self):
+        nodes, edges = self._make_graph()
+
+        dot = Digraph(format="svg", graph_attr={"rankdir": "LR"})
+
+        for n in nodes:
+            # create a value node
+            dot.node(
+                name=str(id(n)),
+                label=f"{{data: {n.data:.4f} | grad: {n.grad:.4f}}}",
+                shape="record",
+            )
+            if n.op:
+                # create an operation node and point the value node to it
+                dot.node(name=str(id(n)) + n.op, label=n.op)
+                dot.edge(str(id(n)) + n.op, str(id(n)))
+
+        for n1, n2 in edges:
+            dot.edge(str(id(n1)), str(id(n2)) + n2.op)
+
+        return dot
